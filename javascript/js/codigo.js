d865b71950a31a44373e0f0755450619650b9770
Jessricardo <jesusapodacaatondo@hotmail.com>
2016-04-12 11:00:56 -0600

    Archivo JS, funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,8 @@
+var botonClic = function ()
+{
+	alert("Clic del botón");
+}
+var nombre="Martin";
+window.document.write("<strong>JS en acción</strong>"+" "+nombre);
+console.log("A punto de lanzar un mensaje");
+alert("Mensaje por pantalla");
