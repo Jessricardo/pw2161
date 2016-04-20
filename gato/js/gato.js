18be8e0817d3a63d0ad9e0b71f0e0a8b36070416
Jessricardo <jesusapodacaatondo@hotmail.com>
2016-04-20 11:01:37 -0600

    Gato falta empate

--- gato/js/gato.js
+++ gato/js/gato.js
@@ -1,6 +1,5 @@
-var contX=0;
-var contO=0;
 var turno=false;
+var ganador="na";
 function clic(ident){
 	if(document.getElementById(ident).innerHTML=="&nbsp;"){
 		if(turno){
@@ -10,30 +9,91 @@ function clic(ident){
 		}
 		turno=!turno;
 	}
-	
+	var contX=0;
+	var contO=0;
 	for(col=1; col<=3;col++){
 		for(fila=1; fila<=3 ;fila++){
-			var casilla="td"+col+fila;
+			var casilla="td"+fila+col;
 			if(document.getElementById(casilla).innerHTML=="X"){
 				contX++;
 			}else if(document.getElementById(casilla).innerHTML=="O"){
 				contO++;
 			}
+			if(contX==3){
+				alert("Gano X");
+				ganador="X";
+				return;
+			}else if(contO==3){
+				alert("Gano O");
+				ganador="O";
+				return;
+			}
 		}
+		contX=0;
+		contO=0;
 	}
+
 	for(fila=1; fila<=3;fila++){
 		for(col=1; col<=3 ;col++){
-			var casilla="td"+col+fila;
+			var casilla="td"+fila+col;
 			if(document.getElementById(casilla).innerHTML=="X"){
 				contX++;
 			}else if(document.getElementById(casilla).innerHTML=="O"){
 				contO++;
 			}
+			if(contX==3){
+				alert("Gano X");
+				ganador="X";
+				return;
+			}else if(contO==3){
+				alert("Gano O");
+				ganador="O";
+				return;
+			}
 		}
+		contX=0;
+		contO=0;
 	}
-	if(contX==3){
-		alert("Gano X");
-	}else if(contO==3){
-		alert("Gano O");
+	var col2=3;
+	for(fila=1; fila<=3;fila++){
+			var casilla="td"+fila+col2;
+			if(document.getElementById(casilla).innerHTML=="X"){
+				contX++;
+			}else if(document.getElementById(casilla).innerHTML=="O"){
+				contO++;
+			}
+			if(contX==3){
+				alert("Gano X");
+				ganador="X";
+				return;
+			}else if(contO==3){
+				alert("Gano O");
+				ganador="O";
+				return;
+			}
+		col2--;
 	}
+	contX=0;
+	contO=0;
+	for(fila=3; fila>=1;fila--){
+			var casilla="td"+fila+fila;
+			if(document.getElementById(casilla).innerHTML=="X"){
+				contX++;
+			}else if(document.getElementById(casilla).innerHTML=="O"){
+				contO++;
+			}
+			if(contX==3){
+				alert("Gano X");
+				ganador="X";
+				return;
+			}else if(contO==3){
+				alert("Gano O");
+				ganador="O";
+				return;
+			}
+	}
+	contX=0;
+	contO=0;
+
+	
 }
