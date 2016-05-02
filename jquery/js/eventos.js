a7b78aea650fd8c13ccf26fc107fc1e145c3d7ec
Jessricardo <jesusapodacaatondo@hotmail.com>
2016-05-02 11:02:36 -0600

    Teclas input ej JQuery

--- jquery/js/eventos.js
+++ jquery/js/eventos.js
@@ -2,16 +2,32 @@
 //$ alias(un sobrenombre)
 //$ == jquery
 //Main
-$(document).on("ready",inicio);
+
 
 var inicio=function()
 {
 	//Preparar los eventos
 	//de todos mis objetos
-	$("#miBoton").on("click",clicBoton);
 	var clicBoton=function()
 	{
 		alert("Clic del botón");
 		$(".anuncioWeb").html("Clic del Botón");
 	}
+	$("#miBoton").on("click",clicBoton);
+	var clicBoton2=function()
+	{
+		alert("Boton 2");
+		
+	}
+	var teclaUnInput=function(tecla)
+	{
+		if(tecla.which == 13){
+			//Que se posicione en otroInput
+			$("#otroInput").focus();
+		}
+	}
+	$("#miBoton").off("click",clicBoton);
+	$("#miBoton").on("click",clicBoton2);
+	$("#unInput").on("keypress",teclaUnInput);
 }
+$(document).on("ready",inicio);
