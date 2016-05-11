115c785d45ea6124dd60fa1539e38383f68e1aa4
Jessricardo <jesusapodacaatondo@hotmail.com>
2016-05-11 11:00:28 -0600

    Validacion de usuario y clave con JS

--- jquery2/js/eventos.js
+++ jquery2/js/eventos.js
@@ -1,6 +1,8 @@
 var iniciaApp=function()
 {
 	var validarEntrada=function(){
+		//
+		event.preventDefault();
 		var usuario=$("#txtUsuario").val();
 		var clave=$("#txtClave").val();
 		if(usuario==""){
@@ -8,6 +10,17 @@ var iniciaApp=function()
 		}
 		if(clave==""){
 			$("#txtClave").focus();
+		}
+		//2.- Verificar usuario y contraseña
+		if(usuario=="pw" && clave=="1234")
+		{
+			$("#datosUsuario").slideUp(700,function(){
+				$("nav").slideDown(700);
+			});
+			
+		}
+		else{
+			alert("Usuario y/o contraseña incorrecta");
 		}
 		console.log("Se disparó el submit");
 	}
